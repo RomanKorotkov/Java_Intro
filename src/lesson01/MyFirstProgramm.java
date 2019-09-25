95dd0a1e6531ed967d6668c35e05d0b6887f3b2f
Roman Korotkov <roman.korotkov@infopulse.com>
2019-09-25 16:26:03 +0300

    first commit

---
+++ src/lesson01/MyFirstProgramm.java
@@ -0,0 +1,74 @@
+package lesson01;
+
+public class MyFirstProgramm {
+    public static void main(String[] args)
+    {
+        //byte to byte
+        byte q = 5;
+        byte w = q;
+        System.out.println(w);
+
+        //byte to short
+        byte e = 6;
+        short r = e;
+        System.out.println(r);
+
+        //byte to char
+        byte t = 45;
+        char y = 50;
+        t = (byte) y;
+        System.out.println(y);
+
+        //byte to int
+        byte u = 50;
+        int i = u;
+        System.out.println(u);
+
+        //byte to long
+        byte o = 89;
+        long p = o;
+        System.out.println(p);
+
+        //byte to float
+        byte a = 5;
+        float s = a;
+        System.out.println(s);
+
+        //byte to double
+        byte d = 10;
+        double f = d;
+        System.out.println(f);
+
+        //short to byte
+        short qq = 32565;
+        byte ww = 15;
+        qq = (short) ww;
+        System.out.println(qq);
+
+        //short to short
+        short ee = 5262;
+        short rr = ee;
+        System.out.println(rr);
+
+        //short to char
+        short tt = 2514;
+        char yy = 569;
+        tt = (short) yy;
+        System.out.println(tt);
+
+        //short to int
+        short uu = 526;
+        int ii = uu;
+        System.out.println(ii);
+
+        //short to long
+        short oo = 178;
+        long pp = oo;
+        System.out.println(pp);
+
+        //short to float
+        short aa = 13256;
+        float ss = aa;
+        System.out.println(ss);
+    }
+}
