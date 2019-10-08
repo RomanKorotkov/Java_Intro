171cff15a5945ee31318359a3b302d258d2c0d28
Roman Korotkov <roman.korotkov@infopulse.com>
2019-10-08 18:51:52 +0300

    home work: Work with strings

---
+++ src/HomeWorkLesson2/WorkWithStrings.java
@@ -0,0 +1,32 @@
+package HomeWorkLesson2;
+
+import java.util.Scanner;
+
+public class WorkWithStrings {
+    public static void main(String[] string) {
+        eachNth();
+    }
+
+    //Variant1
+    private static void eachNth() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please input string");
+        String new_string = in.nextLine();
+        System.out.println("Please input number");
+        int number = in.nextInt();
+        char[] chars = new_string.toCharArray();
+        for (int i = 0; i < chars.length; i = i + number) {
+            System.out.println(chars[i]);
+        }
+
+        //Variant2
+        String miracle = "Miracle";
+        int num1 = 2;
+        char[] chars2 = miracle.toCharArray();
+        for (int i = 0; i < chars2.length; i = i + num1) {
+            System.out.println(chars2[i]);
+
+        }
+    }
+
+}
