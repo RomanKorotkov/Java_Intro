643b8ad12aca8bed61a1b06713833a45b458876e
Roman Korotkov <roman.korotkov@infopulse.com>
2019-10-11 19:46:02 +0300

    Home Work: fibonacci

---
+++ src/HomeWorkLesson2/Fibonacci.java
@@ -0,0 +1,38 @@
+package HomeWorkLesson2;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        printFibonacciNumbers();
+
+    }
+
+    static void printFibonacciNumbers() {
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Please input");
+            int param = in.nextInt();
+            int defoult_param1 = 0;
+            int defoult_param2 = 1;
+
+            if (param < 1) {
+                System.out.println("Please input > 0");
+
+            } else {
+                for (int i = 1; i < param; i = ++i) {
+                    int result = defoult_param1 + defoult_param2;
+                    defoult_param1 = defoult_param2;
+                    defoult_param2 = result;
+                    System.out.println(defoult_param1);
+
+                }
+
+
+            }
+
+        }
+    }
+
+}
+
